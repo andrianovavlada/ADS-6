a64cdfcfc5adb0c0b32cd127c062f800cc5fbbf4
Andrianova Vlada <99177026+andrianovavlada@users.noreply.github.com>
2022-04-13 15:59:58 +0300

    Andrianova IST3

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -5,6 +5,41 @@
 template<typename T, int size>
 class TPQueue {
   // реализация шаблона очереди с приоритетом на кольцевом буфере
+  private:
+    T arr[size];
+    int first, last;
+public:
+    TPQueue() : first(0), last(0) { }
+    void push(T x) {
+        if (last - first >= size) {
+            throw std::string("Full!");
+        } else {
+            int y = last++;
+            while ((--y >= first) && (arr[y % size].prior < x.prior)) {
+                arr[(y++) % size] = arr[y % size];
+            }
+            arr[(y++) % size] = x;
+            }
+ T pop() {
+        return arr[(first++) % size];
+    }
+    int getSize() {
+        return (last - first);
+    }
+    T front() {
+        if (first < last) {
+            return arr[first];
+        } else {
+            throw std::string("Empty!");
+        }
+    }
+    T back() {
+        if (first < last){
+            return arr[last - 1];
+        } else {
+            throw std::string("Empty!");
+        }
+    }
 };
 
 struct SYM {
